f3830099273dc1fd31de549fbb3de5f02cab7a4e
alan247 <alimadejesus247@gmail.com.br>
2024-07-08 09:21:44 -0400

    Cração do Repositório

---
+++ menus/criador.js
@@ -0,0 +1,18 @@
+exports.criado = (comando, prefix, hora, data, pushname, totalcmd) => {
+return `
+╭━─━───༺[🙇🏻‍♀️]༻────━─━╮
+┃[☆]╭
+┃[☆]  ⎙  INFORMAÇÕES DO CRIADOR
+┃[☆]╰
+╰━─━───༺[🙇🏻‍♀️]༻────━─━╯
+┏━━━━━━━━ ✓
+┃[☆] -➤ Criador: NZ°_Dev
+┃[☆] -➤ [ wa.me/556699064658]
+┗━━━━━━━━ ✓
+┏━━━━━━━━ ✓
+┃[☆] -➤ Prefixo : ${prefix}
+┃[☆] -➤ ${botName}
+┗━━━━━━━━ ✓
+`
+ }
+ 
